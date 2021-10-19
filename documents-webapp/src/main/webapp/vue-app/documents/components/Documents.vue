ea22594e980d8ce8f5f372903126395c8d680047
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-10-19 18:19:37 +0100

    Add project skeleton

---
+++ documents-webapp/src/main/webapp/vue-app/documents/components/Documents.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app
+    class="documents-application border-box-sizing transparent"
+    role="main"
+    flat>
+  </v-app>
+</template>
---
+++ documents-webapp/src/main/webapp/vue-app/documents/main.js
@@ -0,0 +1,36 @@
+import './initComponents.js';
+import './extensions.js';
+
+// get overrided components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('Documents');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
+
+document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
+
+const appId = 'DocumentsApplication';
+
+//getting language of the PLF
+const lang = eXo && eXo.env.portal.language || 'en';
+
+//should expose the locale ressources as REST API 
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.Documents-${lang}.json`;
+
+export function init() {
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+    // init Vue app when locale ressources are ready
+    Vue.createApp({
+      template: `<documents id="${appId}" />`,
+      vuetify,
+      i18n
+    }, `#${appId}`, 'Documents');
+  });
+}
