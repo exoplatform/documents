08d16aef16e8c09765dab9df7c31af47a5342e4d
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-10-19 18:19:37 +0100

    Add project skeleton

---
+++ documents-webapp/webpack.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, '.'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+          'eslint-loader',
+        ]
+      }
+    ]
+  },
+  entry: {
+    documents: './src/main/webapp/vue-app/documents/main.js',
+  },
+  output: {
+    path: path.join(__dirname, 'target/documents-portlet/'),
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
+  },
+  externals: {
+    vue: 'Vue',
+    vuetify: 'Vuetify',
+    jquery: '$',
+  },
+};
+
+module.exports = config;
