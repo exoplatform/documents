78806d97341c0e103ee6a251bdb2ec00f99923d6
Moez Diha <mdiha@exoplatform.com>
2021-11-23 15:05:17 +0100

    Fix build failure.

--- documents-services/src/main/java/org/exoplatform/documents/rest/util/EntityBuilder.java
+++ documents-services/src/main/java/org/exoplatform/documents/rest/util/EntityBuilder.java
@@ -98,7 +98,7 @@ public class EntityBuilder {
                              AbstractNodeEntity nodeEntity,
                              List<String> expandProperties) {
     nodeEntity.setId(node.getId());
-    nodeEntity.setName(URLDecoder.decode(node.getName(), StandardCharsets.UTF_8));
+    nodeEntity.setName(node.getName() != null ? URLDecoder.decode(node.getName(), StandardCharsets.UTF_8) : null);
     nodeEntity.setDatasource(node.getDatasource());
     nodeEntity.setDescription(node.getDescription());
     nodeEntity.setAcl(node.getAcl());
