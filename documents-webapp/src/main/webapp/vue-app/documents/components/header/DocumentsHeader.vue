b24116d9c5ae4c84f09e75f8b331f4b7e5ac1c23
Moez Diha <38471028+mdiha@users.noreply.github.com>
2021-11-24 11:59:40 +0100

    Task-50578: Adapt documents mobile view to design. (#41)

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/DocumentsHeader.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/DocumentsHeader.vue
@@ -2,6 +2,17 @@
   <div class="d-flex flex-row">
     <documents-header-left />
     <v-spacer />
-    <documents-header-right />
+    <documents-header-right v-if="!isMobile" />
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    isMobile: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
