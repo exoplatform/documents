f860e91b0ea01eb434c5e78fa66b130dce4fec91
lbengayed <lbengayed@exoplatform.com>
2021-11-25 18:00:38 +0100

    Task-51602: Add screens for documents empty for both desktop and mobile view
    Task-51602: Add screens for documents empty for both desktop and mobile view
    
    Task-51602: Add screens for documents empty for both desktop and mobile view
    
    Task-51602: Add screens for documents empty for both desktop and mobile view

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsAddNewFile.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsAddNewFile.vue
@@ -8,7 +8,7 @@
 export default {
   computed: {
     isMobile() {
-      return this.$vuetify.breakpoint.name === 'xs';
+      return this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'sm';
     },
   },
   created() {
