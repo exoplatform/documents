000c1dfb009492ff67b6eb10ea7a17532c801c19
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2021-12-04 01:07:57 +0100

    Task-50170: Add screens mobile for search documents (#61)
    * Task-50170: Add screens mobile for search documents

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsAddNewFile.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsAddNewFile.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <div v-show="isMobile && !showMobileFilter || !isMobile">
+    <div :class="isMobile && !showMobileFilter || !isMobile ? '' : 'd-none'">
       <v-btn class="btn btn-primary" @click="openDrawer">
         <v-icon :class="!isMobile ? 'me-2' : ''">mdi-plus</v-icon>
         {{ !isMobile ? $t('documents.button.addNewFile') : '' }}
       </v-btn>
     </div>
-    <div v-show="isMobile && showMobileFilter || !isMobile">
+    <div :class="isMobile && showMobileFilter || !isMobile ? '' : 'd-none'">
       <v-icon
-        size="20"
+        size="25"
         class="inputDocumentsFilter text-sub-title pa-1 my-auto "
-        v-show="isMobile && showMobileFilter"
-        @click="$root.$emit('resetSearch')">
+        :class="isMobile && showMobileFilter ? '' : 'd-none'"
+        @click="$root.$emit('document-search', null)">
         fas fa-arrow-left
       </v-icon>
     </div>
