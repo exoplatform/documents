af5ace16fa909b0907a77f0f105ccbd9c9d9439c
Moez Diha <38471028+mdiha@users.noreply.github.com>
2021-12-08 16:53:34 +0100

    Task-7294: Adapt design for adding favorites in mobile. (#65)

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFavoriteCell.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFavoriteCell.vue
@@ -28,10 +28,8 @@
         offset-y
         offset-x>
         <v-list class="pa-0" dense>
-          <v-list-item 
-            class="px-2" 
-            @click.prevent="hitFavoriteButton">
-            <v-list-item-icon class="mr-0 mb-3">
+          <v-list-item class="px-2">
+            <v-list-item-icon class="mr-0">
               <favorite-button
                 :id="fileId"
                 :space-id="spaceId"
