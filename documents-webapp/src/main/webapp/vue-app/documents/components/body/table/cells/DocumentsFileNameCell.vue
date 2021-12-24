3b51e1bdd1edd74831db8567e2a813273b404903
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2021-12-24 15:57:13 +0100

    Task-52584: FixwWrong display of last update date in mobile view (#94)

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileNameCell.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileNameCell.vue
@@ -10,20 +10,22 @@
       v-else
       size="22"
       :color="icon.color">{{ icon.class }}</v-icon>
-    <div class="document-title d-inline-flex width-full" :title="file.name">
-      <div
-        v-sanitized-html="fileName"
-        class="document-name text-truncate hover-underline ms-4">
+    <div>
+      <div class="document-title d-inline-flex" :title="file.name">
+        <div
+          v-sanitized-html="fileName"
+          class="document-name text-truncate hover-underline ms-4">
+        </div>
+        <div
+          v-sanitized-html="fileType"
+          class="document-type hover-underline ms-0">
+        </div>
       </div>
-      <div
-        v-sanitized-html="fileType"
-        class="document-type hover-underline ms-0">
-      </div>
+      <documents-last-updated-cell
+        v-if="isMobile"
+        :file="file"
+        :extension="extension" />
     </div>
-    <documents-last-updated-cell
-      v-if="isMobile"
-      :file="file"
-      :extension="extension" />
   </a>
 </template>
 <script>
