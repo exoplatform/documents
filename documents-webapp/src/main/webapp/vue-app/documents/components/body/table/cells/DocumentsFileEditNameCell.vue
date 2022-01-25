1a1ed165ecb65770bbfec84b2e692383f102fb51
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2022-01-25 16:15:47 +0100

    Task-52859: Fix feedback related to rename document (#125)

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileEditNameCell.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileEditNameCell.vue
@@ -1,37 +1,36 @@
 <template>
-  <div>
-    <v-text-field
-      ref="autoFocusInput1"
-      v-model="file.name"
-      type="text"
-      :rules="nameRules"
-      @focus="editTitle()"
-      @blur="cancelEditNameMode()"
-      class="documentEditName font-weight-bold text-color width-full pt-0"
-      required
-      autofocus
-      outlined
-      dense
-      @keyup="checkInput($event,file.name)">
-      <div slot="append" class="d-flex">
-        <v-divider v-if="!isMobile" vertical />
-        <v-icon
-          class="primary--text ma-1 px-1"
-          small
-          @click="checkInput(13,file.name)">
-          fa-check
-        </v-icon>
-        <v-divider vertical />
-        <v-icon
-          class="clickable ma-1 px-1"
-          color="red"
-          small
-          @click="cancelEditNameMode(file.name)">
-          fa-times
-        </v-icon>
-      </div>
-    </v-text-field>
-  </div>
+  <v-text-field
+    ref="autoFocusInput1"
+    v-model="fileName"
+    height="35px"
+    type="text"
+    :rules="nameRules"
+    @focus="editTitle()"
+    @blur="cancelEditNameMode()"
+    class="documentEditName text-color font-weight-bold width-full pt-0 pl-1"
+    required
+    autofocus
+    outlined
+    dense
+    @keyup="checkInput($event,fileName)">
+    <div slot="append" class="d-flex">
+      <v-divider v-if="!isMobile" vertical />
+      <v-icon
+        class="primary--text ma-1 px-1"
+        small
+        @click="checkInput(13,fileName)">
+        fa-check
+      </v-icon>
+      <v-divider vertical />
+      <v-icon
+        class="clickable ma-1 px-1"
+        color="red"
+        small
+        @click="cancelEditNameMode(fileName)">
+        fa-times
+      </v-icon>
+    </div>
+  </v-text-field>
 </template>
 <script>
 export default {
@@ -43,6 +42,10 @@ export default {
     editNameMode: {
       type: Boolean,
       default: false,
+    },
+    fileName: {
+      type: String,
+      default: '',
     },
     isMobile: {
       type: Boolean,
@@ -76,8 +79,8 @@ export default {
       }
     },
     renameFile(newTitle){
-      if (newTitle!=null){
-        newTitle = newTitle.split('.')[0];
+      if (newTitle){
+        this.file.name = this.fileName;
         this.$attachmentService.getAttachmentById(this.file.id)
           .then(attachment => {
             const path = attachment.path;
