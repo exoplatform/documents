d72f12953fcc307c1a6ce760d1fde8b50585d4f5
lbengayed <lbengayed@exoplatform.com>
2022-02-13 00:18:41 +0100

    Task-54222: Hide download option in folder

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/DuplicateMenuAction.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/DuplicateMenuAction.vue
@@ -9,7 +9,7 @@
     </v-icon>
     <span class="ps-1">{{ $t('documents.label.duplicate') }}</span>
     <v-divider
-      v-if="!isMobile"
+      v-if="!file.folder"
       class="mt-2 dividerStyle" />
   </div>
 </template>
