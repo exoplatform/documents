8fdfb8649b89c54bd36fcbdb2dd0dfa460eaa640
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2022-02-21 15:37:09 +0100

    Task-54186: Add duplicate file action backend (#178)
    * Task-54186: Add duplicate file action backend

--- documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
@@ -79,6 +79,7 @@ export default {
     alert: false,
     alertType: '',
     message: '',
+    ownerId: eXo.env.portal.spaceIdentityId || eXo.env.portal.userIdentityId
   }),
   computed: {
     filesLoad(){
@@ -112,6 +113,7 @@ export default {
     this.$root.$on('document-load-more', this.loadMore);
     this.$root.$on('document-change-view', this.changeView);
     this.$root.$on('document-open-folder', this.openFolder);
+    this.$root.$on('duplicate-document', this.duplicateDocument);
     this.$root.$on('document-search', this.search);
     this.$root.$on('documents-sort', this.sort);
     this.$root.$on('documents-filter', filter => {
@@ -179,6 +181,15 @@ export default {
       this.query = query;
 
       this.refreshFiles();
+    },
+    duplicateDocument(document){
+      this.parentFolderId = document.id;
+      return this.$documentFileService
+        .duplicateDocument(this.parentFolderId,this.ownerId)
+        .then( () => {
+          this.refreshFiles();
+          this.$root.$emit('show-alert', {type: 'success',message: this.$t('documents.alert.success.label.duplicateDocument')});
+        }).catch(e => console.error(e));
     },
     openFolder(parentFolder) {
       this.folderPath='';
