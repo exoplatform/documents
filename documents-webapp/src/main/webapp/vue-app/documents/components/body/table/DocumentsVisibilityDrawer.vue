0d0c6568b5bf98472f1e48a64458364f47560f03
lbengayed <lbengayed@exoplatform.com>
2022-03-24 10:31:19 +0100

    Task-7294: Add section Choice in  visibility drawer

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentsVisibilityDrawer.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentsVisibilityDrawer.vue
@@ -25,6 +25,39 @@
       </v-list-item>
 
       <v-divider dark class="mx-4" />
+
+      <v-list-item>
+        <v-list-item-content class="my-1">
+          <div class="my-4">
+            <v-label for="choice">
+              <span class="font-weight-bold text-start text-color body-2">{{ $t('documents.label.visibility.choice') + ' :' }}</span>
+            </v-label>
+            <v-select
+              v-model="visibilitySelected"
+              :items="visibilityLabel"
+              item-text="text"
+              item-value="value"
+              dense
+              class="caption"
+              outlined
+              :hint="infoMessage"
+              persistent-hint />
+          </div>
+          <div v-if="showSwitch" class="d-flex flex-row align-center my-4">
+            <v-label for="visibility">
+              <span class="text-color body-2">
+                {{ $t('documents.label.visibility.allowEveryone') }}
+              </span>
+            </v-label>
+            <v-spacer />
+            <v-switch
+              v-model="allowEveryone"
+              class="mt-0 me-1" />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider dark class="mx-4" />
     </template>
   </exo-drawer>
 </template>
@@ -43,11 +76,38 @@ export default {
   },
   data: () => ({
     ownerIdentity: [],
+    visibilitySelected: 'allMembers',
+    allowEveryone: true,
   }),
   computed: {
     visibilityTitle(){
       return this.$t('documents.label.visibilityTitle').replace('{0}', this.fileName);
     },
+    visibilityLabel(){
+      return [
+        {
+          text: this.$t('documents.label.visibility.allMembers'),
+          value: 'allMembers',
+        },
+        {
+          text: this.$t('documents.label.visibility.specific'),
+          value: 'specific',
+        },
+      ];
+    },
+    infoMessage(){
+      switch (this.visibilitySelected) {
+      case 'specific':
+        return this.$t('documents.label.visibility.user.info');
+      case 'allMembers':
+        return this.allowEveryone ? this.$t('documents.label.visibility.allMembers.info') : this.$t('documents.label.visibility.specific.info');
+      default:
+        return this.$t('documents.label.visibility.allMembers.info');
+      }
+    },
+    showSwitch(){
+      return this.visibilitySelected === 'allMembers';
+    }
   },
   mounted(){
     this.$userService.getUser(this.file.creatorIdentity.remoteId).then(user => {
