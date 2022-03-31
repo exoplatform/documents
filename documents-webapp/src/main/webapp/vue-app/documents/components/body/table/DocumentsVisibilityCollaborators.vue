b22cbff100869065dacb082f1efbc51189568ca2
lbengayed <lbengayed@exoplatform.com>
2022-03-31 14:37:44 +0100

    Task-55505: Add Collaborator for visibility drawer

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentsVisibilityCollaborators.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentsVisibilityCollaborators.vue
@@ -1,3 +1,19 @@
+<!--
+* Copyright (C) 2022 eXo Platform SAS
+*
+*  This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <gnu.org/licenses>.
+-->
 <template>
   <div class="d-flex">
     <div class="profile-popover user-wrapper pl-4 pt-2 pb-1">
