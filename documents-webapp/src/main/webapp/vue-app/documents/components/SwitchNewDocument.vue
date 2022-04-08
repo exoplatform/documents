f2d43ee9927c5a52beefd8eb907ab88577f8160a
Mohamed Amine Krout <mkrout@exoplatform.com>
2022-04-08 17:41:49 +0100

    Task-54564 : Implement new UI swtich between two apps

--- documents-webapp/src/main/webapp/vue-app/documents/components/SwitchNewDocument.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/SwitchNewDocument.vue
@@ -1,22 +1,22 @@
 <template>
- <div class="switch-application VuetifyApp newAppSwitch">
-  <div data-app="true" class="v-application white v-application--is-ltr theme--light">
-    <div class="v-application--wrap">
-      <div>
-        <div class="switchPosition d-flex flex-row align-center my-4">
-          <v-label for="switch">
-            <span class="text-color labelStyle body-2 pe-2">
-              {{ $t('document.label.newApp') }}
-            </span>
-           </v-label>
-          <v-switch
-            v-model="newApp"
-            class="mt-0 me-1" />
+  <div class="switch-application VuetifyApp newAppSwitch">
+    <div data-app="true" class="v-application white v-application--is-ltr theme--light">
+      <div class="v-application--wrap">
+        <div>
+          <div class="switchPosition d-flex align-center">
+            <v-label for="switch">
+              <span class="text-color labelStyle body-2 pe-1">
+                {{ $t('document.label.newApp') }}
+              </span>
+            </v-label>
+            <v-switch
+              v-model="newApp"
+              class="mt-0 me-1" />
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 <script>
 export default {
