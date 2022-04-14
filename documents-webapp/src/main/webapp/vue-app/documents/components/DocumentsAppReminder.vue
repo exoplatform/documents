a582999fa1778c82a29d6931cadaa16b198fe526
hakermi <hakermi@exoplatform.com>
2022-04-14 16:13:39 +0100

    TASK-56299,56287: Implement Display pop-up Welcome to the new documents app and pop-up Quick access to recent documents
     Implement Display pop-up Welcome to the new documents app and pop-up Quick access to recent documents

---
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsAppReminder.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-main>
+    <changes-reminder :reminder="introduceNewDocAppReminder" />
+    <changes-reminder :reminder="switchOldDocAppReminder" />
+  </v-main>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      introduceNewDocAppReminder: {
+        name: 'IntroduceNewDocApp',
+        title: this.$t('documents.reminder.newDocApp.dialog.title'),
+        img: '/documents-portlet/images/newdocapp.png',
+        description: this.$t('documents.reminder.newDocApp.Dialog.message'),
+      },
+      switchOldDocAppReminder: {
+        name: 'SwitchOldDocuments',
+        title: this.$t('documents.reminder.oldDocSwitch.dialog.title'),
+        img: '/documents-portlet/images/switcholdapp.gif',
+        description: this.$t('documents.reminder.oldDocSwitch.Dialog.message'),
+      },
+    };
+  },
+  mounted() {
+    window.setTimeout(() => {
+      document.dispatchEvent(new CustomEvent('changes-reminder-open'));
+    }, 800);
+  }
+};
+</script>
