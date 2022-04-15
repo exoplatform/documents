4dd27c356c76e4d98562be28702e77a7f37ce163
lbengayed <lbengayed@exoplatform.com>
2022-04-15 10:47:45 +0100

    Task-56124: Fix feedback related to move document

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/views/DocumentsBreadcrumb.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/views/DocumentsBreadcrumb.vue
@@ -26,7 +26,8 @@
               v-on="on"
               @click="openFolder(documents)">
               <a
-                class="caption text-truncate breadCrumb-link"
+                class="caption text-truncate"
+                :id="move ? 'breadCrumb-link-move' : 'breadCrumb-link'"
                 :class="index < documentsBreadcrumb.length-1 && 'path-clickable text-sub-title' || 'text-color not-clickable'">{{ getName(documents.name) }}</a>
             </v-btn>
           </template>
@@ -34,8 +35,8 @@
         </v-tooltip>
         <v-icon
           v-if="index < documentsBreadcrumb.length-1"
-          size="14"
-          class="px-3">
+          :size="move ? 12 : 14"
+          :class="move ? 'px-1' : 'px3'">
           fa-chevron-right
         </v-icon>
       </div>
@@ -45,7 +46,8 @@
         <v-tooltip max-width="300" bottom>
           <template #activator="{ on, attrs }">
             <a
-              class="caption text-sub-title text-truncate path-clickable breadCrumb-link"
+              :id="move ? 'breadCrumb-link-move' : 'breadCrumb-link'"
+              class="caption text-sub-title text-truncate path-clickable"
               :class="documentsBreadcrumb[documentsBreadcrumb.length-1].id === actualFolderId && 'clickable' || ''"
               v-bind="attrs"
               v-on="on"
@@ -53,7 +55,7 @@
           </template>
           <span class="caption">{{ documentsBreadcrumb && documentsBreadcrumb.length && documentsBreadcrumb[0].name }}</span>
         </v-tooltip>
-        <v-icon size="14" class="px-3">fa-chevron-right</v-icon>
+        <v-icon :size="move ? 12 : 14" :class="move ? 'px-1' : 'px3'">fa-chevron-right</v-icon>
       </div>
       <div class="documentss-tree-item long-path-second-item d-flex">
         <v-tooltip bottom>
@@ -70,16 +72,17 @@
             v-for="(documents, index) in documentsBreadcrumb"
             :key="index"
             class="mb-0">
-            <span v-if="index > 0 && index < documentsBreadcrumb.length-2" class="caption"><v-icon size="14" class="tooltip-chevron px-3">fa-chevron-right</v-icon> {{ getName(documents.name) }}</span>
+            <span v-if="index > 0 && index < documentsBreadcrumb.length-2" class="caption"><v-icon :size="move ? 12 : 14" :class="move ? 'tooltip-chevron px-1' : 'tooltip-chevron px3'">fa-chevron-right</v-icon> {{ getName(documents.name) }}</span>
           </p>
         </v-tooltip>
-        <v-icon class="clickable px-3" size="14">fa-chevron-right</v-icon>
+        <v-icon :class="move ? 'clickable px-1' : 'clickable px3'" :size="move ? 12 : 14">fa-chevron-right</v-icon>
       </div>
       <div class="documentss-tree-item long-path-third-item d-flex text-truncate">
         <v-tooltip max-width="300" bottom>
           <template #activator="{ on, attrs }">
             <a
-              class="caption text-sub-title text-truncate path-clickable breadCrumb-link"
+              :id="move ? 'breadCrumb-link-move' : 'breadCrumb-link'"
+              class="caption text-sub-title text-truncate path-clickable"
               :class="documentsBreadcrumb[documentsBreadcrumb.length-1].id === actualFolderId && 'clickable' || ''"
               v-bind="attrs"
               v-on="on"
@@ -87,13 +90,14 @@
           </template>
           <span class="caption">{{ documentsBreadcrumb[documentsBreadcrumb.length-2].name }}</span>
         </v-tooltip>
-        <v-icon size="14" class="px-3">fa-chevron-right</v-icon>
+        <v-icon :size="move ? 12 : 14" :class="move ? 'px-1' : 'px3'">fa-chevron-right</v-icon>
       </div>
       <div class="documentss-tree-item d-flex text-truncate">
         <v-tooltip max-width="300" bottom>
           <template #activator="{ on, attrs }">
             <a
-              class="caption text-truncate breadCrumb-link"
+              :id="move ? 'breadCrumb-link-move' : 'breadCrumb-link'"
+              class="caption text-truncate"
               :class="documentsBreadcrumb[documentsBreadcrumb.length-1].id === actualFolderId && 'text-color' || 'clickable'"
               v-bind="attrs"
               v-on="on"
@@ -116,6 +120,10 @@ export default {
       type: Boolean,
       default: true,
     },
+    move: {
+      type: Boolean,
+      default: false,
+    },
   },
   data: () => ({
     actualFolderId: '',
@@ -123,6 +131,9 @@ export default {
     currentFolderPath: '',
     ownerId: eXo.env.portal.spaceIdentityId || eXo.env.portal.userIdentityId
   }),
+  computed: {
+
+  },
   created() {
     this.$root.$on('set-breadcrumb', data => {
       this.folderPath='';
