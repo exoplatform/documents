5c86ef28fefc8a2ea328c05dd49ace318952b8d9
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-04-18 13:19:22 +0100

    TASK-55810: Documents are not added in folder with uppercase name (#328)
    ISSUE: When open the attachment drawer in default folder param was taking node name as value while the attachment drawer using the title to search the target folder inside its drive or its parent folder.
    FIX: This PR should make sure to send the folder title instead of name in the attachmentdrawer configurations

--- documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
@@ -78,6 +78,7 @@ export default {
     hasMore: false,
     viewExtensions: {},
     currentFolderPath: '',
+    currentFolder: null,
     groupsSizes: {
       'thisDay': 0,
       'thisWeek': 0,
@@ -138,7 +139,7 @@ export default {
     this.$root.$on('confirm-document-deletion', this.deleteDocument);
     this.$root.$on('undo-delete-document', this.undoDeleteDocument);
     this.$root.$on('documents-open-drawer', this.openDrawer);
-    this.$root.$on('set-current-folder-url', this.setFolderUrl);
+    this.$root.$on('set-current-folder', this.setCurrentFolder);
     this.$root.$on('cancel-add-folder', this.cancelAddFolder);
     this.$root.$on('document-search', this.search);
     this.$root.$on('save-visibility', this.saveVisibility);
@@ -459,7 +460,7 @@ export default {
         if (pathparts.length>1){
           attachmentAppConfiguration= {
             'sourceApp': 'NEW.APP',
-            'defaultFolder': decodeURI(pathparts[1]),
+            'defaultFolder': this.extractDefaultFolder(pathparts[1]),
             'defaultDrive': {
               isSelected: true,
               name: `.spaces.${eXo.env.portal.spaceGroup}`,
@@ -475,7 +476,7 @@ export default {
         if (pathparts.length>1){
           attachmentAppConfiguration= {
             'sourceApp': 'NEW.APP',
-            'defaultFolder': decodeURI(pathparts[1]),
+            'defaultFolder': this.extractDefaultFolder(pathparts[1]),
             'defaultDrive': {
               isSelected: true,
               name: 'Personal Documents',
@@ -486,8 +487,13 @@ export default {
       }
       document.dispatchEvent(new CustomEvent('open-attachments-app-drawer', {detail: attachmentAppConfiguration}));
     },
-    setFolderUrl(url) {
-      this.currentFolderPath=url;
+    extractDefaultFolder(targetPath) {
+      const path = decodeURI(targetPath);
+      const folderName = path && path.substring(path.lastIndexOf('/'));
+      return folderName && path.replace(folderName, `/${this.currentFolder.name}`);
+    },
+    setCurrentFolder(folder) {
+      this.currentFolder = folder;
     },
     getDocumentDataFromUrl() {
       const currentUrlSearchParams = window.location.search;
