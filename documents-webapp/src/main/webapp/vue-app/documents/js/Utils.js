6c290a41eaf3512525eb58c87d9b40b9de5208e8
sofyenne <shajchedli@exoplatform.com>
2022-07-22 16:45:19 +0100

    Add the getFolderPath method

--- documents-webapp/src/main/webapp/vue-app/documents/js/Utils.js
+++ documents-webapp/src/main/webapp/vue-app/documents/js/Utils.js
@@ -1,16 +1,33 @@
+function getFolderPath(path){
+  if (eXo.env.portal.spaceName){
+    const index = path.indexOf('/Documents');
+    if (index !== -1){
+      return path.substring(index+10);
+    }
+  } else {
+    if (path.includes('/Private')){
+      const index  = path.indexOf('/Private');
+      if (index !== -1){
+        return path.substring(index);
+              
+      }
+    }
+    if (path.includes('/Public')){
+      const index = path.indexOf('/Public');
+      if (index !== -1){
+        return path.substring(index);
+      }
+    } 
+  }
+}
 export function getFolderpreviewPathFrombreadCrumb(breadCrumb){
-  let ParentpathFolder ='';
-  let isSource = true;
+
   for (const folderName in breadCrumb){
     const portalBaseURL= breadCrumb[folderName].slice(0,breadCrumb[folderName].indexOf('?'));
-    const pathFolder = `${portalBaseURL}/${ParentpathFolder}${folderName}`;
+    const path = getFolderPath(decodeURIComponent(breadCrumb[folderName].slice(breadCrumb[folderName].indexOf('?')+1,breadCrumb[folderName].indexOf('&'))));
+    const pathFolder = `${portalBaseURL}${path}`;
     breadCrumb[folderName]=pathFolder ;
-    ParentpathFolder = `${folderName}/` ;
-    if (isSource){
-      breadCrumb[folderName]= portalBaseURL;
-      ParentpathFolder = '';
-      isSource= false ;
-    }
   }
   return breadCrumb;
 }
+
