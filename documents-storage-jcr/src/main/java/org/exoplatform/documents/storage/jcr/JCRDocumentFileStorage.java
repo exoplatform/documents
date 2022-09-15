exoplatform/documents

commit d49d2229ccc1e1a6f69dabd4ead9f7385edea4f8

Task-58643: Fix rename document uploaded (#416)
Task-58643: Fix rename document uploaded

documents-storage-jcr/src/main/java/org/exoplatform/documents/storage/jcr/JCRDocumentFileStorage.java
@@ -529,14 +529,13 @@ public class JCRDocumentFileStorage implements DocumentFileStorage {
       }
       String name = Text.escapeIllegalJcrChars(cleanString(title));
       //clean node name
-      name = cleanString(name);
 
       name = URLDecoder.decode(name,"UTF-8");
 
       String oldName = node.getName();
       if (oldName.indexOf('.') != -1 && node.isNodeType(NodeTypeConstants.NT_FILE)) {
         String ext = oldName.substring(oldName.lastIndexOf('.'));
-        title = title.concat(ext);
+        title = name.concat(ext);
         name = name.concat(ext);
       }
 

documents-storage-jcr/src/main/java/org/exoplatform/documents/storage/jcr/util/JCRDocumentsUtil.java
@@ -534,10 +534,10 @@ public class JCRDocumentsUtil {
     int i = 0;
     while (i < strLength) {
       char c = cleanedStr.charAt(i);
-      if (c == '/' || c == ':' || c == '[' || c == ']' || c == '*' || c == '\'' || c == '"' || c == '|' || c == 'ʿ' || c == 'ˇ') {
+      if (c == '/' || c == ':' || c == '[' || c == ']' || c == '*' || c == '\'' || c == '"' || c == '|' || c == 'ʿ' || c == 'ˇ' || c == '.') {
         cleanedStr.deleteCharAt(i);
         cleanedStr.insert(i, '_');
-      } else if (!(Character.isLetterOrDigit(c) || Character.isWhitespace(c) || c == '.' || c == '-' || c == '_')) {
+      } else if (!(Character.isLetterOrDigit(c) || Character.isWhitespace(c) || c == '-' || c == '_')) {
         cleanedStr.deleteCharAt(i);
         strLength = cleanedStr.length();
         continue;
@@ -547,7 +547,7 @@ public class JCRDocumentsUtil {
     while (org.apache.commons.lang.StringUtils.isNotEmpty(cleanedStr.toString()) && !Character.isLetterOrDigit(cleanedStr.charAt(0))) {
       cleanedStr.deleteCharAt(0);
     }
-    String clean = cleanedStr.toString().toLowerCase();
+    String clean = cleanedStr.toString();
     if (clean.endsWith("-")) {
       clean = clean.substring(0, clean.length()-1);
     }

commit 1f25d8ae0ab444f4b624e938e9f378158a5a44ce

TASK-59433: Rename file : accentuated charachters aren't saved (#469)
Prior to change there is a regression caused by #416
So the modification is reverted, no need to change the title since it is a property that does not need to be cleaned nor escaped its illegal JCR characters.

documents-storage-jcr/src/main/java/org/exoplatform/documents/storage/jcr/JCRDocumentFileStorage.java
@@ -546,7 +546,7 @@ public class JCRDocumentFileStorage implements DocumentFileStorage {
       String oldName = node.getName();
       if (oldName.indexOf('.') != -1 && node.isNodeType(NodeTypeConstants.NT_FILE)) {
         String ext = oldName.substring(oldName.lastIndexOf('.'));
-        title = name.concat(ext);
+        title = title.concat(ext);
         name = name.concat(ext);
       }
 