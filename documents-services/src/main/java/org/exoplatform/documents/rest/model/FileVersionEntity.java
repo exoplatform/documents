74596c44ce4b8a996d3e751b0a87236b0718cd25
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-10-05 15:58:57 +0100

    feat: Edit summary version - EXO-59624  (#529)
    Allow to edit the summary version of a document

--- documents-services/src/main/java/org/exoplatform/documents/rest/model/FileVersionEntity.java
+++ documents-services/src/main/java/org/exoplatform/documents/rest/model/FileVersionEntity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022 eXo Platform SAS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <gnu.org/licenses>.
+ */
 package org.exoplatform.documents.rest.model;
 
 import lombok.AllArgsConstructor;
@@ -12,6 +28,10 @@ import java.util.Date;
 public class FileVersionEntity {
 
   private String  id;
+
+  private String  frozenId;
+
+  private String  originId;
 
   private int     versionNumber;
 
