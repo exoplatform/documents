8c3932a7bef8355b1eb4ae502a494c0a8a4ca82f
Mohamed Amine Krout <mkrout@exoplatform.com>
2022-10-14 10:03:08 +0100

    feat: When searching a tag from document the detail drawer is still displayed - EXO-60218 (#558)

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentInfoDrawer.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentInfoDrawer.vue
@@ -220,6 +220,7 @@ export default {
         this.file.versionNumber++;
       }
     });
+    window.addEventListener('search-metadata-tag', this.close);
   },
   methods: {
     showVersionHistory() {
