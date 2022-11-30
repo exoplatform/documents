51ec83378862b4a89edbe22c1a5311dbf0fb0174
Mohamed Amine Krout <mkrout@exoplatform.com>
2022-11-30 12:09:06 +0100

    feat: Filter rules - EXO-60866 (#608)

--- documents-storage-jcr/src/test/java/org/exoplatform/documents/storage/jcr/JCRDocumentFileStorageTest.java
+++ documents-storage-jcr/src/test/java/org/exoplatform/documents/storage/jcr/JCRDocumentFileStorageTest.java
@@ -307,7 +307,7 @@ public class JCRDocumentFileStorageTest {
     doCallRealMethod().when(JCRDocumentsUtil.class, "getSortDirection", filter);
     jcrDocumentFileStorage.getFolderChildNodes(filter, identity, 0, 0);
     verify(documentSearchServiceConnector,
-           times(1)).appSearch(identity, "collaboration", "/documents/path", filter, 0, 0, "lastUpdatedDate", "ASC");
+           times(1)).search(identity, "collaboration", "/documents/path", filter, 0, 0, "lastUpdatedDate", "ASC");
 
   }
 
