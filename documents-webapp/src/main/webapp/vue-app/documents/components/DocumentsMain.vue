f8f776226f8c5203da6d89442de706904e38e969
Mohamed Amine Krout <mkrout@exoplatform.com>
2022-12-16 17:25:04 +0100

    feat:  Analytics for extended search - EXO-60884 (#634)

--- documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
@@ -702,6 +702,7 @@ export default {
           operation: 'extendedSearch',
           parameters: {
             spaceId: eXo.env.portal.spaceId,
+            origin: 'Portlet document',
             view: this.selectedView === 'timeline' ? 'recentView': 'folderView',
           },
           timestamp: Date.now()
