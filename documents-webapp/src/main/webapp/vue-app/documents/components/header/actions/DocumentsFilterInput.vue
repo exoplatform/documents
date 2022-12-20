10414e1b75e58b8a2c7ea3fd678f38a8000008b8
Mohamed Amine Krout <mkrout@exoplatform.com>
2022-12-20 16:17:32 +0100

    feat: Optimize search launching - EXO-60874 (#637)
    * feat: Optimize search launching - EXO-60874

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsFilterInput.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsFilterInput.vue
@@ -1,18 +1,55 @@
 <template>
   <div>
-    <common-search-input
-      ref="commonInput"
-      :query="query"
-      @filterQuery="filterQuery"
-      @cancelSearch="cancelSearch" />
+    <v-icon
+      size="16"
+      class="inputDocumentsFilter text-sub-title pa-1 my-auto mt-2"
+      v-show="isMobile && !showMobileFilter"
+      @click="mobileFilter()">
+      fas fa-filter
+    </v-icon>
+    <v-text-field
+      v-model="query"
+      ref="inputQuery"
+      :placeholder="$t('documents.label.filterDocuments')"
+      v-show="isMobile && showMobileFilter || !isMobile"
+      :append-icon="appendIcon"
+      prepend-inner-icon="fa-filter"
+      class="inputDocumentsFilter pa-1 my-auto width-full"
+      @click:append="cancelSearch" />
   </div>
 </template>
 <script>
 export default {
   data: () => ({
     query: null,
+    startSearchAfterInMilliseconds: 300,
+    showMobileFilter: false,
+    timeout: null,
   }),
-
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'sm';
+    },
+    appendIcon() {
+      return this.query && 'mdi-close primary--text' || null;
+    },
+  },
+  watch: {
+    query() {  
+      if (!this.query) {
+        if (this.timeout) {
+          window.clearTimeout(this.timeout);
+        } 
+        this.$root.$emit('document-search', this.query);
+        return;
+      }
+      this.$root.$emit('set-loading',true);this.$root.$emit('set-loading',true);
+      if (this.timeout) {
+        window.clearTimeout(this.timeout);
+      } 
+      this.waitForEndTyping(); 
+    },
+  },
   created() {
     this.$root.$on('resetSearch', this.resetSearch);
     this.$root.$on('filer-query', this.filterQuery);
@@ -23,10 +60,24 @@ export default {
         return;
       }
       this.query = query;
-      this.$root.$emit('document-search', this.query);
+      this.$root.$emit('document-search', this.query);     
+    },
+    mobileFilter(){
+      this.showMobileFilter = !this.showMobileFilter;
+      this.$root.$emit('show-mobile-filter', this.showMobileFilter);
     },
     cancelSearch(){
       this.query = null;
+      this.$refs.inputQuery.blur();
+    },
+    resetSearch(){
+      this.cancelSearch();
+      this.mobileFilter();
+    },
+    waitForEndTyping() {
+      this.timeout = window.setTimeout(() => {        
+        this.$root.$emit('document-search', this.query);         
+      }, this.startSearchAfterInMilliseconds);
     },
   },
 };
