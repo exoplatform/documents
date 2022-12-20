e3e88d05b6c62258d5fe6c9718fa64cfc2b984f1
Krout Mohamed Amine <mkrout@exoplatform.com>
2022-12-20 16:16:55 +0100

    feat: Optimize search launching - EXO-60874

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsFilterInput.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsFilterInput.vue
@@ -22,7 +22,7 @@
 export default {
   data: () => ({
     query: null,
-    startSearchAfterInMilliseconds: 700,
+    startSearchAfterInMilliseconds: 300,
     showMobileFilter: false,
     timeout: null,
   }),
