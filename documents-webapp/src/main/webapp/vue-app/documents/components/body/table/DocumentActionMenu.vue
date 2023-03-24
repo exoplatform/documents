d3f6abc37f935792edf911081c665b3192939c40
Jihed Chabbeh <31882754+Jihed525@users.noreply.github.com>
2023-03-24 09:18:48 +0100

    fix: Remove 3-dots menu actions role redactor filter -EXO-63325  (#829)
    Prior to this change, 3-dots menu actions are filtered by a redactor role inside a redactor space.
    This PR removes the redactor role actions filter and enhance the extensions filtering.

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentActionMenu.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/DocumentActionMenu.vue
@@ -29,11 +29,7 @@ export default {
     menuExtensionApp: 'DocumentMenu',
     menuExtensionType: 'menuActionMenu',
     menuExtensions: {},
-    mobileOnlyExtensions: ['favorite','details'],
-    desktopOnlyExtensions: ['edit'],
     editExtensions: ['edit'],
-    fileOnlyExtension: ['download','favorite'],
-    redactorExtension: ['download', 'versionHistory','details', 'copyLink'],
     sharedDocumentSuspended: true,
     downloadDocumentSuspended: true,
     supportedDocuments: null
@@ -41,11 +37,7 @@ export default {
   created() {
     document.addEventListener(`extension-${this.menuExtensionApp}-${this.menuExtensionType}-updated`, this.refreshMenuExtensions);
     document.addEventListener('documents-supported-document-types-updated', this.refreshSupportedDocumentExtensions);
-    this.$transferRulesService.getDocumentsTransferRules().then(rules => {
-      this.sharedDocumentSuspended = rules.sharedDocumentStatus === 'true';
-      this.downloadDocumentSuspended = rules.downloadDocumentStatus === 'true';
-      this.refreshMenuExtensions();
-    });
+    this.refreshMenuExtensions();
     this.refreshSupportedDocumentExtensions();
   },
   computed: {
@@ -61,51 +53,18 @@ export default {
     }
   },
   methods: {
-    checkTransferRules(extension) {
-      if (extension.id === 'download') {
-        return !this.downloadDocumentSuspended;
-      }
-      if (extension.id === 'visibility') {
-        return !this.sharedDocumentSuspended;
-      }
-      return true;
-    },
-    refreshSupportedDocumentExtensions () {
+    refreshSupportedDocumentExtensions() {
       this.supportedDocuments = extensionRegistry.loadExtensions('documents', 'supported-document-types');
     },
     refreshMenuExtensions() {
       let extensions = extensionRegistry.loadExtensions(this.menuExtensionApp, this.menuExtensionType);
-      if (!this.file.canAdd){
-        extensions = extensions.filter(extension => this.redactorExtension.includes(extension.id));    }
 
       if (!this.fileCanEdit) {
         extensions = extensions.filter(extension => !this.editExtensions.includes(extension.id));
       }
-      if (this.file.path.includes('News Attachments')) {
-        extensions = extensions.filter(extension => extension.id !== 'visibility');
-      }
-      if (this.file.cloudDriveFolder) {
-        extensions = extensions.filter(extension => extension.id === 'copyLink');
-      }
-      extensions = extensions.filter(extension => this.checkTransferRules(extension)
-                                                     && extension.enabled(this.file));
-     
+      extensions = extensions.filter(extension => extension.enabled(this.file));
 
-      let changed = false;
-      extensions.forEach(extension => {
-        if (extension.id && (!this.menuExtensions[extension.id] || this.menuExtensions[extension.id] !== extension)) {
-          if (((!this.isMobile && !this.mobileOnlyExtensions.includes(extension.id))
-              || (this.isMobile && !this.desktopOnlyExtensions.includes(extension.id)))
-          && (!this.file.folder || (this.file.folder && !this.fileOnlyExtension.includes(extension.id)))) {
-            this.menuExtensions[extension.id] = extension;
-            changed = true;
-          }
-        }
-      });
-      // force update of attribute to re-render switch new extension id
-      if (changed) {
-        this.menuExtensions = Object.assign({}, this.menuExtensions);
-      }
+      this.menuExtensions = extensions;
     },
   }
 };
