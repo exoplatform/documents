e4cf7e4c602b98f7e5979243c6a6fd371577437c
Ali HAMDI <ahamdi@exoplatform.com>
2023-04-18 15:48:01 +0100

    fix: navigate between folders using history button of browser - EXO-63527 (#886)
    Browsing history backward/forward with Docuents application was not working because the variables used to retrieve and list documents inside a folder were not correctly updated when back/forward buttons are pressed.
    This fix updates correctly the variables and manages also the case when the selected folder is the root.

--- documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
@@ -1089,7 +1089,7 @@ export default {
     setCurrentFolder(folder) {
       this.currentFolder = folder;
     },
-    getDocumentDataFromUrl() {
+    getDocumentDataFromUrl(path) {
       const currentUrlSearchParams = window.location.search;
       const queryParams = new URLSearchParams(currentUrlSearchParams);
       if (!eXo.env.portal.spaceName && queryParams.has('userId')) {
@@ -1116,11 +1116,15 @@ export default {
         const path = queryParams.get('path') || '';
         this.selectFile(path);
       } else {
-        const path = window.location.pathname;
+        if (!path) {
+          path = window.location.pathname;
+        }
         const pathParts  = path.split( `${eXo.env.portal.selectedNodeUri.toLowerCase()}/`);
         if (pathParts.length > 1) {
           this.folderPath = pathParts[1];
           this.selectedView = 'folder';
+        } else {
+          this.folderPath = '';
         }
         if (queryParams.has('view')) {
           const view = queryParams.get('view');
@@ -1138,11 +1142,12 @@ export default {
       }
       return this.$nextTick();
     },
-    onBrowserNavChange() {
+    onBrowserNavChange(e) {
       this.resetSelections();
-      this.getDocumentDataFromUrl();
+      this.getDocumentDataFromUrl(e.currentTarget.location.pathname);
+      this.parentFolderId = null;
       this.refreshFiles()
-        .finally(() => this.$root.$emit('update-breadcrumb'));
+        .finally(() => this.$root.$emit('update-breadcrumb', this.folderPath));
     },
     displayMessage(message, persist) {
       this.message = message.message;
