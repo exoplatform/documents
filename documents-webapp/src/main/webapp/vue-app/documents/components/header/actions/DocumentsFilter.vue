d151336aba44cbb5a22b43474dfb0c9329f2f627
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-05-15 10:50:18 +0100

    feat: Optimise Filters UI and UX  - EXO-64101 (#917)

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsFilter.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/actions/DocumentsFilter.vue
@@ -2,7 +2,7 @@
   <v-scale-transition>
     <select
       id="filterDocumentsSelect"
-      v-model="filterDocumentsSelected"
+      v-model="primaryFilter"
       v-if="!isMobile"
       name="documentsFilter"
       class="selectPrimaryFilter input-block-level ignore-vuetify-classes  pa-0 my-auto ml-2"
@@ -31,16 +31,19 @@ export default {
       type: Boolean,
       default: false
     },
+    primaryFilter: {
+      type: String,
+      default: 'all',
+    },
   },
   data: () => ({
-    filterDocumentsSelected: 'All',
-    filterDocuments: [{name: 'All'},{name: 'Favorites'}],
+    filterDocuments: [{name: 'all'},{name: 'favorites'}],
     query: '',
     extended: false,
   }),
   created() {
     this.$root.$on('set-documents-filter', data => {
-      this.filterDocumentsSelected= data;
+      this.primaryFilter= data;
     });
     this.$root.$on('set-documents-search', data => {
       this.query= data.query;
@@ -50,7 +53,7 @@ export default {
   computed: {
     filterNumber(){
       let fNum = 0;
-      if (this.filterDocumentsSelected.toLowerCase()!=='all') {
+      if (this.primaryFilter.toLowerCase()!=='all') {
         fNum++;
       }
       if (this.extended && this.query) {
@@ -67,8 +70,8 @@ export default {
   },
   methods: {
     changeDocumentsFilter(){
-      this.$root.$emit('documents-filter', this.filterDocumentsSelected.toLowerCase());
-      this.$root.$emit('set-mobile-filter', this.filterDocumentsSelected);
+      this.$root.$emit('documents-filter', this.primaryFilter.toLowerCase());
+      this.$root.$emit('set-mobile-filter', this.primaryFilter);
     },
     openDrawer(){
       this.$root.$emit('open-mobile-filter-menu',true);
