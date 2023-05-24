00c8216c60edb23e77b6d594a2b564e92f5b2978
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2023-05-24 14:04:40 +0100

    feat: Implement documents public links - EXO-64307,EXO-64319 (#922)
    Implement documents public links

---
+++ documents-services/src/main/java/org/exoplatform/documents/entity/PublicDocumentAccessEntity.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2023 eXo Platform SAS
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <gnu.org/licenses>.
+ */
+package org.exoplatform.documents.entity;
+
+import lombok.Data;
+import org.exoplatform.commons.api.persistence.ExoEntity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity(name = "PublicDocumentAccess")
+@ExoEntity
+@Table(name = "DOCUMENTS_PUBLIC_ACCESS")
+@Data
+@NamedQuery(name = "PublicDocumentAccess.getTokenByNodeId", query = "SELECT DISTINCT c FROM PublicDocumentAccess c where c.nodeId = :nodeId")
+public class PublicDocumentAccessEntity implements Serializable {
+
+  @Id
+  @SequenceGenerator(name = "SEQ_DOCUMENT_TOKEN_ID", sequenceName = "SEQ_DOCUMENT_TOKEN_ID", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_DOCUMENT_TOKEN_ID")
+  @Column(name = "ID", nullable = false)
+  private Long    id;
+
+  @Column(name = "NODE_ID", nullable = false)
+  private String nodeId;
+
+  @Column(name = "TOKEN", nullable = false)
+  private String  token;
+
+  @Column(name = "EXPIRATION_DATE")
+  private Date    expirationDate;
+
+  @Column(name = "HAS_PASSWORD", nullable = false)
+  private boolean hasPassword;
+}
