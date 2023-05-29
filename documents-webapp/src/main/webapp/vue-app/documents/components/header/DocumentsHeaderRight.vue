6ee1110ee9ed38adc79c309333afa5e3a10a79d9
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-05-29 10:38:43 +0100

    feat: Optimise Filters UI and UX  - EXO-64101 (#954)
    * feat: Optimise Filters UI and UX  - EXO-64101

--- documents-webapp/src/main/webapp/vue-app/documents/components/header/DocumentsHeaderRight.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/header/DocumentsHeaderRight.vue
@@ -6,7 +6,7 @@
     class="d-flex flex-no-wrap flex-row-reverse documents-header-right-actions"
     parent-element="div"
     element="div"
-    element-class="mx-auto ma-lg-0 documents-header-right-action" />
+    element-class="ma-lg-0 documents-header-right-action" />
 </template>
 <script>
 
