a1375841ef18edd4e926fae5453cb6e4552ae191
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2023-06-15 16:57:30 +0100

    feat: Fix download one folder action - EXO-64605 (#988)
    Prior to this change, downalod one folder wasn't implemented or handled out of multi-selection.
    This PR makes sure to handle the download of one folder

--- documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
@@ -348,6 +348,7 @@ export default {
     this.$root.$on('upload-new-version-action-invoke', this.uploadNewVersion);
     this.$root.$on('copy-public-access-link', this.getDocumentPublicAccessLink);
     this.$root.$on('mark-document-as-viewed', this.markDocumentAsViewed);
+    this.$root.$on('documents-folder-download', this.downloadFolder);
     document.addEventListener('move-dropped-documents', this.handleMoveDroppedDocuments);
     document.addEventListener('document-open-folder-to-drop', this.handleOpenFolderToDrop);
   },
@@ -355,6 +356,10 @@ export default {
     document.removeEventListener(`extension-${this.extensionApp}-${this.extensionType}-updated`, this.refreshViewExtensions);
   },
   methods: {
+    downloadFolder(file) {
+      this.selectedDocuments.push(file);
+      this.bulkDownloadDocument();
+    },
     markDocumentAsViewed(file) {
       document.dispatchEvent(new CustomEvent('mark-attachment-as-viewed', {detail: {file: file}}));
     },
