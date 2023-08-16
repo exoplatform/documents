252c9eec1949bda8821ccad9fb219f68b4e3bd68
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2023-08-16 15:34:58 +0100

    fix: Notify only corresponding collaborators after making changes on their permission on the document managed access drawer - EXO-65096 (#1077) (#1078)
    After implementing this previous pull request https://github.com/exoplatform/documents/pull/1069 to notify only the corresponding collaborators after making changes to their permissions, space members are still receiving notifications.
    
    With this new change, we will first verify whether the linked node has already been created with the same permission list before notifying the member.

--- documents-services/src/test/java/org/exoplatform/documents/service/DocumentFileServiceTest.java
+++ documents-services/src/test/java/org/exoplatform/documents/service/DocumentFileServiceTest.java
@@ -447,7 +447,15 @@ public class DocumentFileServiceTest {
     when(identityManager.getIdentity("1")).thenReturn(socialIdentity);
     documentFileService.updatePermissions("123", nodePermission, 1L);
     verify(documentFileStorage, times(1)).updatePermissions("123", nodePermission, identity);
-    verify(documentFileStorage, times(1)).shareDocument("123", 1L, true);
+    verify(documentFileStorage, times(1)).shareDocument("123", 1L, false);
+    //
+    Map<Long, String> toNotify = new HashMap<>();
+    toNotify.put(1L, "read");
+    nodePermission.setToNotify(toNotify);
+    documentFileService.updatePermissions("123", nodePermission, 1L);
+    verify(documentFileStorage, atLeast(1)).updatePermissions("123", nodePermission, identity);
+    verify(documentFileStorage, atLeast(1)).shareDocument("123", 1L, true);
+
   }
 
   @Test
