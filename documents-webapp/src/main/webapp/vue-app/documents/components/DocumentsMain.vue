e5ae480944d019463335fbe414a1191f5929fcbe
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-09-18 09:28:27 +0100

    feat: Integration of new app header component - EXO-64660 (#1097)

--- documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/DocumentsMain.vue
@@ -111,6 +111,9 @@
         ref="documentVersionHistory" />
       <document-action-context-menu />
       <public-document-options-drawer />
+      <documents-add-new-menu-mobile
+        ref="documentAddItemMenu"
+        :is-mobile="isMobile" />
       <v-file-input
         id="uploadVersionInput"
         class="d-none"
@@ -276,6 +279,9 @@ export default {
       this.resetSelections();
       this.cancelBulkAction(actionId);
     });
+    this.$root.$on('open-add-new-mobile', this.displayAddMenuMobile);
+    this.$root.$on('close-add-new-mobile', this.hideAddMenuMobile);
+   
     this.$root.$on('documents-refresh-files', this.refreshFiles);
     this.$root.$on('openTreeFolderDrawer', this.folderTreeDrawer);
 
@@ -1625,7 +1631,13 @@ export default {
       const folderPath = destFolder.path.split(`Groups${space.groupId}`)[1].replace('/Documents', '/documents');
       const pathName = `${eXo.env.portal.context}/g/${space.groupId.replaceAll('/', ':')}/${space.prettyName}`;
       window.location.replace(`${pathName}${folderPath}?view=folder`, window.location.pathname);
-    }
+    },
+    displayAddMenuMobile() {
+      this.$refs.documentAddItemMenu.open();
+    }, 
+    hideAddMenuMobile() {
+      this.$refs.documentAddItemMenu.close();
+    },
   },
 };
 </script>
