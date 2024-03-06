ce31fefde83e6ca69a5027d73e07b8e92a9541e9
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2024-03-06 16:54:03 +0100

    feat: Remove DocumentsExtensions portlet from bottom-all-container configuration - EXO-70137 (#1210)
    Prior to this change, in bottom-all-container, we inject and load documents extensions as a portlet, so, it is loaded on all pages even if no need to be used which can lead to loading performance issues.
    To fix that, we must use js load-group, which are now available through all webapps to load documents extensions only with favorite drawer.

--- documents-webapp/src/main/webapp/vue-app/documents-extensions/initComponents.js
+++
@@ -1,8 +0,0 @@
-import DocumentsFavoriteItem from './components/DocumentsFavoriteItem.vue';
-const components = {
-  'documents-favorite-item': DocumentsFavoriteItem,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
