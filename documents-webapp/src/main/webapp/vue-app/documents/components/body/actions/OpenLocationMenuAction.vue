e1442e298accbd88eee61c95f0ea3683b51a98a2
Mohamed Amine Krout <mkrout@exoplatform.com>
2024-03-08 11:09:10 +0100

    feat: Documents_basic actions_US: Go to location - EXO-70176 (#1213)

---
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/OpenLocationMenuAction.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="clickable pt-1 mx-2"
+    @click="openLocation()">
+    <v-icon
+      size="13"
+      class="pe-1 iconStyle">
+      fa-external-link-alt
+    </v-icon>
+    <span class="ps-1">{{ $t('document.label.go.location') }}</span>
+    <v-divider
+      v-if="!file.cloudDriveFolder"
+      class="mt-1 dividerStyle" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    file: {
+      type: Object,
+      default: null,
+    }
+  },
+  methods: {
+    openLocation() {
+      this.$root.$emit('open-folder-by-id', this.file.parentFolderId);
+    },
+  },
+};
+</script>./OpenLocationMenuAction.vue
