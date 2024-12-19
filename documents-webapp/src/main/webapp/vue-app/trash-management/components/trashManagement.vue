20a54527850acb29d9ba3a1f264ab0279f0aa137
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2024-12-19 12:29:40 +0100

    feat: Add documents trash management portlet - EXO-76028 (#1319)

---
+++ documents-webapp/src/main/webapp/vue-app/trash-management/components/trashManagement.vue
@@ -0,0 +1,32 @@
+<!--
+ * Copyright (C) 2024 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+-->
+<template>
+  <v-app>
+    <v-card class="application-body" flat>
+      <trash-management-toolbar />
+      <documents-trash-list
+        ref="documentsTrashList"
+        class="px-5" />
+    </v-card>
+  </v-app>
+</template>
+<script>
+export default {
+
+};
+</script>
