5b005f9732d0073b7a62f89e20b19d0c7af7e53d
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-04-02 14:32:00 +0100

    feat: Use new meeds attachment viewer on documents app - EXO-78042
    This change will Use new meeds attachment viewer on documents app

--- documents-webapp/src/main/webapp/vue-app/attachment/components/Attachment.vue
+++ documents-webapp/src/main/webapp/vue-app/attachment/components/Attachment.vue
@@ -24,7 +24,6 @@
         :supported-documents="supportedDocuments"
         :attachments="attachments"
         :open-attachments-in-editor="openAttachmentsInEditor" />
-      <attachments-preview-dialog />
     </div>
   </v-app>
 </template>
