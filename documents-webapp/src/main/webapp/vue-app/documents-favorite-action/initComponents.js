329914c346e1d1627a0825acea8f2a9aef7b5c4e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-06-20 13:22:55 +0100

    feat: Centralize Documents Favorite Action Behavior (#1437)

--- documents-webapp/src/main/webapp/vue-app/documents-favorite-action/initComponents.js
+++ documents-webapp/src/main/webapp/vue-app/documents-favorite-action/initComponents.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 eXo Platform SAS.
+ * Copyright (C) 2025 eXo Platform SAS.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -14,12 +14,13 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
-import './initComponents.js';
-import '../documents-favorite-action/initComponents.js';
-import {initDocumentsExtensions} from './extensions.js';
-import '../documents-icons-extension/extensions.js';
 
-export function init() {
-  initDocumentsExtensions();
+import DocumentsFavoriteButton from './components/DocumentsFavoriteButton.vue';
+
+const components = {
+  'documents-favorite-button': DocumentsFavoriteButton,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
 }
--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFavoriteCell.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFavoriteCell.vue
@@ -3,11 +3,10 @@
     class="align-center"
     v-show="display"
     :id="`favorite-cell-file-${fileId}`">
-    <documents-favorite-action
+    <documents-favorite-button
       :file="file"
-      :is-mobile="isMobile"
-      @added="handleFavoriteDocumentAdded"
-      @removed="handleFavoriteDocumentRemoved" />
+      @added="favoriteAdded"
+      @removed="favoriteRemoved" />
   </div>
 </template>
 <script>
@@ -31,80 +30,28 @@ export default {
     },
   },
   data: () => ({
-    fileToDownload: false,
-    handle: null,
+    isFavorite: false,
   }),
   computed: {
     fileId() {
-      return this.file && this.file.id;
-    },
-    spaceId() {
-      return eXo.env.portal.spaceId || 0;
-    },
-    isFavorite() {
-      return this.file && this.file.metadatas && this.file.metadatas.favorites && this.file.metadatas.favorites.length;
+      return this.file?.id;
     },
     display() {
       return !this.isMobile && !this.file?.folder && (this.isFavorite || this.hover);
     },
   },
   created() {
-    this.$root.$on('documents-offline-favorite-sync-clear', this.setNoDownload);
-    this.$root.$on('documents-offline-settings-updated', this.downloadFile);
     this.init();
-  },
-  beforeDestroy() {
-    this.$root.$off('documents-offline-favorite-sync-clear', this.setNoDownload);
-    this.$root.$off('documents-offline-settings-updated', this.downloadFile);
   },
   methods: {
     init() {
-      this.isFavorite = this.file && this.file.metadatas && this.file.metadatas.favorites && this.file.metadatas.favorites.length;
+      this.isFavorite = !!this.file?.metadatas?.favorites?.length;
     },
-    setNoDownload() {
-      this.fileToDownload = true;
+    favoriteAdded() {
+      this.isFavorite = true;
     },
-    downloadFile() {
-      if (this.fileToDownload) {
-        if (this.$root.isFavoritesSynchronized) {
-          this.handleFavoriteDocumentAdded();
-        } else {
-          window.setTimeout(() => this.handleFavoriteDocumentAdded(), 200);
-        }
-      }
-    },
-    async handleFavoriteDocumentRemoved() {
-      document.dispatchEvent(new CustomEvent('alert-message', {detail: {
-        alertType: 'success',
-        alertMessage: this.$t('documents.favoriteRemoved'),
-      }}));
-      if (this.$root.isFavoritesSynchronized) {
-        await this.$documentOfflineService.removeFile(this.file);
-      }
-    },
-    async handleFavoriteDocumentAdded() {
-      if (this.$root.isFavoritesSynchronized || !this.$root.pwaEnabled) {
-        document.dispatchEvent(new CustomEvent('alert-message', {detail: {
-          alertType: 'success',
-          alertMessage: this.$t('documents.favoriteAdded'),
-        }}));
-        if (this.$root.isFavoritesSynchronized) {
-          await this.$documentOfflineService.saveFile(this.file);
-        }
-      } else {
-        document.dispatchEvent(new CustomEvent('alert-message', {detail: {
-          alertType: 'success',
-          alertMessage: this.$t('documents.favoriteAddedWithSyncChoice'),
-          alertLinkCallback: this.handleSyncFavoriteDocument,
-          alertLinkText: this.$t('documents.file.synchronizeLocally'),
-        }}));
-      }
-    },
-    handleSyncFavoriteDocument() {
-      this.$root.$emit('documents-offline-favorite-sync-clear');
-      this.$root.$emit('documents-offline-settings-open');
-      this.$root.$emit('close-alert-message');
-      this.fileToDownload = true;
+    favoriteRemoved() {
+      this.isFavorite = false;
     },
   },
 };
