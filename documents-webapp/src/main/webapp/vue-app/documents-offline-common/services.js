9001bbed6c7fd656b02f07c517896bf03cdb3877
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-06-20 15:28:30 +0100

    fix: Fix Accessing to Local Folder Path from Offline Application (#1439)

--- documents-webapp/src/main/webapp/vue-app/documents-offline-common/services.js
+++ documents-webapp/src/main/webapp/vue-app/documents-offline-common/services.js
@@ -15,7 +15,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import * as documentOfflineService from '../../js/DocumentOfflineService.js';
+import * as documentOfflineService from './js/DocumentOfflineService.js';
 
 if (!Vue.prototype.$documentOfflineService) {
   window.Object.defineProperty(Vue.prototype, '$documentOfflineService', {
