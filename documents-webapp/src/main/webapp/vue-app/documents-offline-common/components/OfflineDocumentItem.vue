9249403cbd79fcc759f84b275828e106493b1c02
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-06-30 16:39:30 +0100

    feat: Allow accessing info drawer from offline docs drawer - EXO-79907 - EXIP-15 This change will allow to access Documents Info drawer from Offline Docs Drawer.

--- documents-webapp/src/main/webapp/vue-app/documents-offline-common/components/OfflineDocumentItem.vue
+++ documents-webapp/src/main/webapp/vue-app/documents-offline-common/components/OfflineDocumentItem.vue
@@ -15,108 +15,122 @@
  along with this program. If not, see <http://www.gnu.org/licenses/>.
 -->
 <template>
-  <tr>
-    <td :class="cellClass">
-      <v-card
-        class="d-flex align-center overflow-hidden"
-        color="transparent"
-        flat
-        @click="openFile">
-        <v-icon
-          :color="fileIconColor"
-          size="24">
-          {{ fileIcon }}
-        </v-icon>
-        <div
-          :title="file.name"
-          class="d-flex overflow-hidden ps-5">
-          <span class="text-truncate">{{ file.name }}</span>
+  <v-hover v-model="hover">
+    <tr>
+      <td :class="cellClass">
+        <div class="d-flex align-center overflow-hidden">
+          <v-card
+            class="d-flex align-center overflow-hidden"
+            color="transparent"
+            flat
+            @click="openFile">
+            <v-icon
+              :color="fileIconColor"
+              size="24">
+              {{ fileIcon }}
+            </v-icon>
+            <div
+              :title="file.name"
+              class="d-flex overflow-hidden ps-5">
+              <span class="text-truncate">{{ file.name }}</span>
+            </div>
+          </v-card>
+          <v-spacer />
+          <documents-offline-info-button
+            v-if="infoIcon && hover"
+            :file-id="file.id" />
         </div>
-      </v-card>
-    </td>
-    <td v-if="!noDates" :class="cellClass">
-      <v-tooltip bottom>
-        <template #activator="{on, attrs}">
-          <div
-            class="text-center"
-            v-bind="attrs"
-            v-on="on">
-            <date-format :value="file.modifiedDate" />
-          </div>
-        </template>
-        <date-format
-          :value="file.modifiedDate"
-          :format="fullDateFormat" />
-      </v-tooltip>
-    </td>
-    <td v-if="!noDates" :class="cellClass">
-      <v-tooltip bottom>
-        <template #activator="{on, attrs}">
-          <div
-            class="text-center"
-            v-bind="attrs"
-            v-on="on">
-            <date-format :value="file.downloadTime" />
-          </div>
-        </template>
-        <date-format
-          :value="file.downloadTime"
-          :format="fullDateFormat" />
-      </v-tooltip>
-    </td>
-    <td
-      :class="cellClass"
-      :width="actionsCellWidth"
-      class="px-0">
-      <div class="d-flex justify-end align-center">
-        <v-tooltip v-if="accessBadge && offlineAccessTime" bottom>
-          <template #activator="{on, attrs}">
-            <v-btn
-              v-bind="attrs"
-              v-on="on"
-              :aria-label="$t('OfflineApp.pwa.documents.accessedWhileOffline')"
-              class="me-4"
-              small
-              icon
-              @click="markAsUpdated">
-              <v-avatar
-                class="error-color-background"
-                size="12" />
-            </v-btn>
-          </template>
-          <span>{{ $t('OfflineApp.pwa.documents.accessedWhileOffline') }}</span>
-        </v-tooltip>
-        <v-tooltip v-if="allowUpload" bottom>
-          <template #activator="{on, attrs}">
-            <v-btn
-              v-bind="attrs"
-              v-on="on"
-              :aria-label="$t('OfflineApp.pwa.uploadVersion')"
-              :loading="uploading"
-              class="me-4"
-              icon
-              @click="uploadVersion">
-              <v-icon>fa-upload</v-icon>
-            </v-btn>
-          </template>
-          <span>{{ $t('OfflineApp.pwa.uploadVersion') }}</span>
-        </v-tooltip>
+      </td>
+      <td
+        v-if="!noDates"
+        :class="cellClass"
+        width="175px">
         <v-tooltip bottom>
           <template #activator="{on, attrs}">
-            <v-btn
+            <div
+              class="text-center"
               v-bind="attrs"
-              v-on="on"
-              :aria-label="canPreview ? $t('OfflineApp.pwa.preview') : $t('OfflineApp.pwa.download')"
-              icon
-              @click="openFile">
-              <v-icon>{{ canPreview ? 'fa-eye' : 'fa-download' }}</v-icon>
-            </v-btn>
+              v-on="on">
+              <date-format :value="file.modifiedDate" />
+            </div>
           </template>
-          <span>{{ canPreview ? $t('OfflineApp.pwa.preview') : $t('OfflineApp.pwa.download') }}</span>
+          <date-format
+            :value="file.modifiedDate"
+            :format="fullDateFormat" />
         </v-tooltip>
-      </div>
-    </td>
-  </tr>
+      </td>
+      <td
+        v-if="!noDates"
+        :class="cellClass"
+        width="175px">
+        <v-tooltip bottom>
+          <template #activator="{on, attrs}">
+            <div
+              class="text-center"
+              v-bind="attrs"
+              v-on="on">
+              <date-format :value="file.downloadTime" />
+            </div>
+          </template>
+          <date-format
+            :value="file.downloadTime"
+            :format="fullDateFormat" />
+        </v-tooltip>
+      </td>
+      <td
+        :class="cellClass"
+        :width="actionsCellWidth"
+        class="ps-0">
+        <div class="d-flex justify-end align-center">
+          <v-tooltip v-if="accessBadge && offlineAccessTime" bottom>
+            <template #activator="{on, attrs}">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                :aria-label="$t('OfflineApp.pwa.documents.accessedWhileOffline')"
+                class="me-4"
+                small
+                icon
+                @click="markAsUpdated">
+                <v-avatar
+                  class="error-color-background"
+                  size="12" />
+              </v-btn>
+            </template>
+            <span>{{ $t('OfflineApp.pwa.documents.accessedWhileOffline') }}</span>
+          </v-tooltip>
+          <v-tooltip v-if="allowUpload" bottom>
+            <template #activator="{on, attrs}">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                :aria-label="$t('OfflineApp.pwa.uploadVersion')"
+                :loading="uploading"
+                class="me-4"
+                icon
+                @click="uploadVersion">
+                <v-icon>fa-upload</v-icon>
+              </v-btn>
+            </template>
+            <span>{{ $t('OfflineApp.pwa.uploadVersion') }}</span>
+          </v-tooltip>
+          <v-tooltip bottom>
+            <template #activator="{on, attrs}">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                :aria-label="canPreview ? $t('OfflineApp.pwa.preview') : $t('OfflineApp.pwa.download')"
+                icon
+                @click="openFile">
+                <v-icon>{{ canPreview ? 'fa-eye' : 'fa-download' }}</v-icon>
+              </v-btn>
+            </template>
+            <span>{{ canPreview ? $t('OfflineApp.pwa.preview') : $t('OfflineApp.pwa.download') }}</span>
+          </v-tooltip>
+        </div>
+      </td>
+    </tr>
+  </v-hover>
 </template>
 <script>
 export default {
@@ -141,6 +155,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    infoIcon: {
+      type: Boolean,
+      default: false,
+    },
   },
   data: () => ({
     fullDateFormat: {
@@ -150,6 +168,7 @@ export default {
       hour: 'numeric',
       minute: 'numeric',
     },
+    hover: false,
     uploading: false,
   }),
   computed: {
