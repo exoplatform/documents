3262084acad746a1360dfc14e7c9a19164865f19
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-07-07 16:07:12 +0100

    feat: Allow to Open a JCR Document In Desktop using MS Office - EXIP-17 (#1498)
    This change will allow to open Office Documents in Desktop Applications using Digest authentication which will require a generated password in order to validate user credentials.

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/OpenInDesktopMenuAction.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/OpenInDesktopMenuAction.vue
@@ -30,6 +30,10 @@
       <v-icon size="16">{{ icon }}</v-icon>
     </v-card>
     <span class="ps-1 text-body menu-text-color">{{ $t('documents.label.openInDesktop') }}</span>
+    <open-in-desktop-credentials-dialog
+      v-if="open"
+      ref="dialog"
+      :href="href" />
   </v-card>
 </template>
 <script>
@@ -40,6 +44,9 @@ export default {
       default: null,
     },
   },
+  data: () => ({
+    open: false,
+  }),
   computed: {
     iconExtension() {
       return this.$documentsIconsExtension?.[0]?.get?.(this.file?.mimeType);
