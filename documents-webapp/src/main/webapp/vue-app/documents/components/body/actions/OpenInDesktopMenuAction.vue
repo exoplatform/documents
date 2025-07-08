fbea9e68233ab48826138bfad955f344dc1dc87c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-07-08 11:57:38 +0100

    fix: Fix Opening Dialog using Right Click - EXIP-17 (#1504)
    Prior to this change, when opening the 'Open In Desktop' dialog using the right click, the open gets closed when clicking anywhere else, due to the fact that the Menu is destroyed at any click. This change will move the dialog into Main Area to avoid making the dialog display depends on Menu display.

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/OpenInDesktopMenuAction.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/actions/OpenInDesktopMenuAction.vue
@@ -30,10 +30,6 @@
       <v-icon size="16">{{ icon }}</v-icon>
     </v-card>
     <span class="ps-1 text-body menu-text-color">{{ $t('documents.label.openInDesktop') }}</span>
-    <open-in-desktop-credentials-dialog
-      v-if="open"
-      ref="dialog"
-      :href="href" />
   </v-card>
 </template>
 <script>
@@ -44,9 +40,6 @@ export default {
       default: null,
     },
   },
-  data: () => ({
-    open: false,
-  }),
   computed: {
     iconExtension() {
       return this.$documentsIconsExtension?.[0]?.get?.(this.file?.mimeType);
