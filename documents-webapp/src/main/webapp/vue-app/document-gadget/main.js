598beef0bf6eee91f5a6e74bdf8a32a67c461a8c
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-07-16 15:25:44 +0200

    feat: Display documents list drawer for document gadget- EXO-80122 - eXIP-18 (#1552)
    Fix feedbacks

--- documents-webapp/src/main/webapp/vue-app/document-gadget/main.js
+++ documents-webapp/src/main/webapp/vue-app/document-gadget/main.js
@@ -64,15 +64,6 @@ export function init(settings) {
           getDownloadUrl(file) {
             return this.$documentsUtils.getDownloadUrl(file.id, file.lastModified);
           },
-          previewDocument(file) {
-            const files = [];
-            this.files.forEach((item) => {
-              if (!item.folder && Vue.prototype?.$supportedDocuments.filter(doc =>doc.mimeType === item.mimeType).length === 0){
-                files.push({'id': item.id,'filename': item.name,'mimetype': item.mimeType,'source': 'documents','downloadUrl': `/rest/v1/documents/content/${item.id}`, 'icon': this.getFileIcon(item)});}
-            }
-            );
-            document.dispatchEvent(new CustomEvent('open-attachments-preview', {detail: {'attachments': files,'id': file.id }}));
-          },
           openInEditMode(file) {
             const fileId = file.sourceID ? file.sourceID : file.id;
             window.open(`${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/oeditor?docId=${fileId}&backTo=${window.location.pathname}`, '_blank');
