8d5c781b88ac04f523bf24269fbc1183cdb9743e
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-07-21 12:37:49 +0200

    feat: Placeholder when no documents - EXO-80369 - eXIP-18 (#1562)
    This change will add a placeholder when no documents displayed in drawer

---
+++ documents-webapp/src/main/webapp/vue-app/document-gadget/components/DocumentListEmptyMessage.vue
@@ -0,0 +1,36 @@
+<!--
+ Copyright (C) 2025 eXo Platform SAS.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+-->
+<template>
+  <div class="d-flex flex-column align-center justify-center pa-6 mt-2">
+    <v-icon
+      size="60"
+      color="secondary">
+      fas fa-file-alt
+    </v-icon>
+    <div class="pa-5">{{ title }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
