54d99c26fe4c3f8e56160024d9ad689cc3d4fb5a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-08-13 16:45:52 +0100

    feat: Implement Documents WebDav User Settings UI - exoplatform/exip#21 (#1594)
    This change will implement the UI which will allow to generate an API Key for the end user in order to mount a WebDav Drive using OTP verification process.

--- documents-webapp/src/main/webapp/vue-app/documents-user-setting/initComponents.js
+++ documents-webapp/src/main/webapp/vue-app/documents-user-setting/initComponents.js
@@ -1,7 +1,36 @@
+/*
+ * Copyright (C) 2025 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import DocumentsUserSettings from './components/DocumentsUserSettings.vue';
+
+import DocumentsCredentialInputs from './components/common/DocumentsCredentialInputs.vue';
+
+import DocumentsWebdavDrawer from './components/drawers/DocumentsWebdavDrawer.vue';
+import DocumentsWebdavConfirmAccessDrawer from './components/drawers/DocumentsWebdavConfirmAccessDrawer.vue';
+import DocumentsWebdavMapDrivesDrawer from './components/drawers/DocumentsWebdavMapDrivesDrawer.vue';
+import DocumentsWebdavRegenerateAccessDrawer from './components/drawers/DocumentsWebdavRegenerateAccessDrawer.vue';
 
 const components = {
   'documents-user-settings': DocumentsUserSettings,
+  'documents-credential-inputs': DocumentsCredentialInputs,
+  'documents-webdav-drawer': DocumentsWebdavDrawer,
+  'documents-webdav-confirm-access-drawer': DocumentsWebdavConfirmAccessDrawer,
+  'documents-webdav-map-drives-drawer': DocumentsWebdavMapDrivesDrawer,
+  'documents-webdav-regenerate-access-drawer': DocumentsWebdavRegenerateAccessDrawer,
 };
 
 for (const key in components) {
