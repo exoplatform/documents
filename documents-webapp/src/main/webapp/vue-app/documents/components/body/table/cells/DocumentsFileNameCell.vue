b782b25e26c49d9b9f37e4a27e6902ea5a3b7a8b
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-08-19 13:46:58 +0200

    fix: Fix video file Display - EXO-80878 (#1607)
    This change will fix display of video file.

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileNameCell.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileNameCell.vue
@@ -252,6 +252,9 @@ export default {
     menuActionTooltip() {
       return this.$t('documents.label.menu.action.tooltip');
     },
+    canEdit() {
+      return this.file?.acl?.canEdit;
+    },
   },
   created() {
     $(document).on('mousedown', (event) => {
@@ -333,9 +336,9 @@ export default {
         this.$root.ownerId = this.file.identityId;
         this.$root.spaceId = this.file.spaceId;
         this.$root.$emit('document-open-folder', this.file);
-      } else if (this.isFileEditable && this.file?.acl?.canEdit)  {
+      } else if (this.isFileEditable && this.canEdit) {
         this.openInEditMode(this.file);
-      } else if (this.isFileOnlyReadable || !this.file?.acl?.canEdit)  {
+      } else if (this.isFileOnlyReadable) {
         this.openInReadOnlyMode(this.file);
       } else {
         this.$root.$emit('documents-preview', this.file);
