dd4c3a5c2fb579df6939090f76b8e7b433e77616
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-08-28 17:12:58 +0100

    feat: eXIP19_US01.02: Folder Tree view for personal drive - EXO-80384
    This commit will add Personal Drive View by listing drives of the last visited space of the current user in the tree view.

--- documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileNameCell.vue
+++ documents-webapp/src/main/webapp/vue-app/documents/components/body/table/cells/DocumentsFileNameCell.vue
@@ -332,7 +332,8 @@ export default {
       this.loading = true;
       if (this.file?.folder) {
         this.$root.$emit('document-open-folder', this.file);
-      } if (this.file?.drive) {
+      }
+      if (this.file?.drive) {
         this.$root.ownerId = this.file.identityId;
         this.$root.spaceId = this.file.spaceId;
         this.$root.$emit('document-open-folder', this.file);
