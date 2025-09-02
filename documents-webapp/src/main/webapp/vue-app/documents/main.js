c7e14300c94b4f70d723288fe49b93dedc153298
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-09-02 11:48:42 +0200

    feat: No cards view for docs list when mobile - EXO-81031 - eXIP-24 (#1645)
    This change will hide cards view option for mobile view

--- documents-webapp/src/main/webapp/vue-app/documents/main.js
+++ documents-webapp/src/main/webapp/vue-app/documents/main.js
@@ -101,6 +101,9 @@ export async function init(appId, canEdit,  settings, settingsSaveUrl) {
       categoryDepth() {
         return this.settings.categoryDepth || 4;
       },
+      isMobile() {
+        return this.$vuetify.breakpoint.smAndDown;
+      },
     },
     watch: {
       async selectedCategoryId() {
