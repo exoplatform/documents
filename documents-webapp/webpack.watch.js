ec0902c6320837f1e14a205929d4b8e6633f81e0
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-09-08 10:46:16 +0100

    Update documents-webapp/webpack.watch.js
    Co-authored-by: Boubaker Khanfir <bkhanfir@exoplatform.com>

--- documents-webapp/webpack.watch.js
+++ documents-webapp/webpack.watch.js
@@ -5,7 +5,7 @@ const webpackProductionConfig = require('./webpack.prod.js');
 
 module.exports = merge(webpackProductionConfig, {
   output: {
-    path: 'D:\\\eXo\\servers\\platform-7.1.x-meeds-qaui-SNAPSHOT/webapps/documents-portlet/',
+    path: '/exo-server/webapps/documents-portlet/',
     filename: 'js/[name].bundle.js'
   },
   mode: 'development',
