839f37881f30d7a54937300ac7843d56f71cd37f
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2026-06-18 11:41:29 +0200

    fix: Use correct drive URL in share notification for personal documents - EXO-86459 (#1949)
    Fix feedbacks

--- documents-services/src/main/java/org/exoplatform/documents/notification/utils/NotificationUtils.java
+++ documents-services/src/main/java/org/exoplatform/documents/notification/utils/NotificationUtils.java
@@ -69,7 +69,7 @@ public class NotificationUtils {
       String groupId = space.getGroupId().replace("/", ":");
       stringBuilder.append("g/").append(groupId).append("/").append(spacePrettyName).append("/documents");
     } else {
-      stringBuilder.append(portalOwner).append("/dashboard/drive");
+      stringBuilder.append(portalOwner).append("/documents");
     }
     boolean isTargetNodeFile = isNodeFile(sharedNode);
     if (isTargetNodeFile) {
